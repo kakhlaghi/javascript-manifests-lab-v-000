66b3ba227d73f353f2410407103f25f22763514e
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-27 17:01:04 +0000

    automatically backed up by learn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= main
+//= require main
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
